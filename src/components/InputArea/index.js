24e1b7838d6e5e0b5ffd04cc698f6c302654dec0
thasneemmanaf <123thasneemsalim@gmail.com>
2020-12-11 23:40:52 +0100

    Add state for inputArea component

--- src/components/InputArea/index.js
+++ src/components/InputArea/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import styles from "./InputArea.module.css";
 
-function InputArea() {
-	return <textarea className={styles.inputArea} rows="5" cols="40"></textarea>;
+function InputArea({ inputText, setInputText }) {
+	const onChangeHandler = (e) => {
+		setInputText(e.target.value);
+	};
+
+	return (
+		<textarea
+			className={styles.inputArea}
+			placeholder="Please enter your joke here"
+			rows="5"
+			cols="40"
+			value={inputText}
+			onChange={onChangeHandler}></textarea>
+	);
 }
 
 export default InputArea;
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import InputArea from "../../components/InputArea";
 import Button from "../../components/Button";
 import OutputArea from "../../components/OutputArea";
 import styles from "./HomePage.module.css";
 
 function HomePage() {
+	const [inputText, setInputText] = useState("");
+
 	return (
 		<div>
 			<div className={styles.btns}>
 				<Button>Generate a Random Joke</Button>
 			</div>
 
-			<InputArea />
+			<InputArea inputText={inputText} setInputText={setInputText} />
 			<div className={styles.btns}>
 				<Button>Encode</Button>
 				<Button>Decode</Button>
