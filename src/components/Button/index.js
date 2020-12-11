f761a4a71c8c1d297bcbca3123d321269bd1056d
thasneemmanaf <123thasneemsalim@gmail.com>
2020-12-12 00:24:02 +0100

    Implement Rovarspraket

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Button.module.css";
+import encodeText from "../../utils/encodeText";
 
 function Button({ children, type, inputText, setOutputText }) {
 	const onClickHandler = (type) => {
 		if (type === "encode") {
-			setOutputText(inputText);
+			setOutputText(encodeText(inputText));
 		}
 	};
 
---
+++ src/utils/encodeText.js
@@ -0,0 +1,16 @@
+const encodeText = (text) => {
+	let encodedText = "";
+	// "y" is considered as a vowel in swedish
+	const consonants = "bcdfghjklmnpqrstvwxz";
+	for (let char of text) {
+		let smallChar = char.toLowerCase();
+		encodedText += char;
+		//  Check if the character is a consonant
+		if (consonants.includes(smallChar)) {
+			encodedText = encodedText + "o" + char;
+		}
+	}
+	return encodedText;
+};
+
+export default encodeText;
