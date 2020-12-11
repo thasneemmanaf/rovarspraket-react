018b571784ce8303cd1c77002a8fa2df0bacc3a4
thasneemmanaf <123thasneemsalim@gmail.com>
2020-12-11 22:51:01 +0100

    Add button componet

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import InputArea from "../../components/InputArea";
+import Button from "../../components/Button";
+import styles from "./HomePage.module.css";
 
 function HomePage() {
 	return (
 		<div>
+			<div className={styles.btns}>
+				<Button />
+			</div>
+
 			<InputArea />
+			<div className={styles.btns}>
+				<Button />
+				<Button />
+			</div>
 		</div>
 	);
 }
